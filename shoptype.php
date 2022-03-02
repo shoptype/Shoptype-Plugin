d88e52a00f25ac4910524e0f6c1d6dad7bf8c147
astroajay <astroajay@gmail.com>
2022-03-02 21:30:30 +0530

    bumped the version and added the site url and name globals (not replaced the variable itself)

--- shoptype.php
+++ shoptype.php
@@ -3,7 +3,7 @@
 Plugin Name:  Shoptype
 Plugin URI:    
 Description:  Integrate shoptype directly into your network with native login, checkout, market, product features and native integrations with budypress social features. 
-Version:      1.0
+Version:      1.2.0
 Author:       shoptype 
 Author URI:   https://www.shoptype.com
 License:      GPL2
@@ -20,6 +20,8 @@ function shoptype_header(){
 	global $stRefcode;
 	global $productUrl;
 	global $brandUrl;
+	global $siteUrl;
+	global $siteName;
   echo '<script src="https://cdn.jsdelivr.net/gh/shoptype/Shoptype-JS@2.7.6/shoptype.js"></script>';
   echo "<awakesetup apikey='$stApiKey' refcode='$stRefcode' cartcountmatch='.wcmenucart-details' platformid='$stPlatformId'></awakesetup>";
   echo "<awakeMarket platformid='$stPlatformId' productpage='$productUrl' brandPage='$brandUrl'></awakeMarket>";
