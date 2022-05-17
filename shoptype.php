662398b8c12dfb59c4f26abca4b54eeaefd263bd
kuruvilla <kuruvillac@gmail.com>
2022-05-17 17:50:43 +0530

    Fixed the issue with the cart count not inc

--- shoptype.php
+++ shoptype.php
@@ -28,12 +28,13 @@ function shoptype_header(){
 	global $siteUrl;
 	global $siteName;
 	global $loginUrl;
+	global $cartCountMatch;
 
 	$siteUrl = get_site_url();
 	$siteName = get_bloginfo('name');
 	
   echo '<script src="https://cdn.jsdelivr.net/gh/shoptype/Shoptype-JS@2.7.8/shoptype.js"></script>';
-  echo "<awakesetup apikey='$stApiKey' refcode='$stRefcode' cartcountmatch='.wcmenucart-details' platformid='$stPlatformId' loginUrl='$loginUrl'></awakesetup>";
+  echo "<awakesetup apikey='$stApiKey' refcode='$stRefcode' cartcountmatch='$cartCountMatch' platformid='$stPlatformId' loginUrl='$loginUrl'></awakesetup>";
   echo "<awakeMarket platformid='$stPlatformId' productpage='$productUrl' brandPage='$brandUrl'></awakeMarket>";
   echo '<script src="https://cdn.jsdelivr.net/gh/shoptype/Awake-Market-JS@1.6/awakeMarket.min.js"></script>';
 };
