c79368d5d0c03882da062bcf80f6da1149859854
astroajay <astroajay@gmail.com>
2022-06-29 16:30:21 +0530

    bumped up plugin version

--- shoptype.php
+++ shoptype.php
@@ -3,7 +3,7 @@
 Plugin Name:  Shoptype
 Plugin URI:    
 Description:  Integrate shoptype directly into your network with native login, checkout, market, product features and native integrations with budypress social features. 
-Version:      1.4.1
+Version:      1.4.2
 Author:       shoptype 
 Author URI:   https://www.shoptype.com
 License:      GPL2
