91b020d63e928dc711bc9e228b9e8ba7ce1068a0
kuruvilla <kuruvillac@gmail.com>
2023-01-26 11:51:05 +0530

    Added the delete button fixed the filter menu

--- templates/page-products-home.php
+++ templates/page-products-home.php
@@ -1,73 +1,7 @@
 <?php
-/* Template Name:Shoptype Products Home */
+/* Template Name:Products Home */
 get_header();
 ?>
-<div id="filterContainer" class="menu-main">
-	<div class="menu-container" id="st-filter">
-		<div class="menu-title">
-			<h3 class="menu-title-heading">Filter Menu</h3>
-		</div>
-		<div class="menu-list">
-			<div id="menuOptionList" class="menu-options">
-				<div class="menu-filters">	
-					<?php
-					if(isset($stFilterJson)){
-					$stFilters = json_decode($stFilterJson);
-						foreach ($stFilters as $filter) {
-						?>
-							<div class="menu-brand-select">
-							<div class="menu-option-block1">
-							<h4 class="menu-option-title"><?php echo $filter->name ?></h4>
-							</div>
-							<select name="<?php echo $filter->name ?>" key="<?php echo $filter->key ?>" id="<?php echo str_replace(" ","-",$filter->name) ?>" class="menu-option-select" <?php echo $filter->multi; ?>>
-							<?php foreach ($filter->values as $filterValue) {	?>
-							<option value="<?php echo $filterValue->value ?>"><?php echo $filterValue->name ?></option>
-							<?php } ?>
-							</select>
-							</div>
-
-							<?php
-						}
-					}
-					?>
-					<div class="menu-brand-select">
-						<div class="menu-option-block1">
-							<h4 class="menu-option-title">Sort By</h4>
-						</div>
-						<select name="sortBy" key="sortBy" id="sortBy" class="menu-option-select">
-							<option value="">None</option>
-							<option value="price">Price</option>
-							<option value="createdAt">Latest</option>
-							<option value="quantitySold">Most Sold</option>
-						</select>
-					</div>
-					<div class="menu-brand-select">
-						<div class="menu-option-block1">
-							<h4 class="menu-option-title">Sort Order</h4>
-						</div>
-						<select name="sortOrder" key="orderBy" id="sortOrder" class="menu-option-select">
-							<option value="asc">ascending</option>
-							<option value="desc">descending</option>
-						</select>
-						<select name="sortOrder" key="currency" id="sortOrder" class="menu-option-select" hidden>
-							<option value="<?php echo $stDefaultCurrency;?>"><?php echo $stDefaultCurrency;?></option>
-						</select>		 
-					</div>
-				</div>
-			</div>
-		</div>
-		<div class="menu-apply-div">
-			<div class="menu-apply-button">
-				<h3 class="menu-apply-button-lable" onclick="clearFilters()">Reset</h3>
-			</div>
-			<div class="menu-apply-button">
-				<h3 class="menu-apply-button-lable" onclick="filterProducts()">Apply &amp; Refresh</h3>
-			</div>
-		</div>
-	</div>
-	<div class="st-filter-btn" onclick="toggleFilter()"><img src="<?php echo $path ?>/images/Filter-Icon.png" loading="lazy" alt="" class="st-filter-img"></div>
-</div>
-
 <script>
 	let options={};
 	let myshop_offset=10;
@@ -94,15 +28,15 @@ get_header();
 	}
 	
 	function toggleFilter(){
-		var btn = document.getElementById("filterContainer");
-		if(btn.style.left=="0px"){
-			btn.style.left="-350px";
+		var filter = document.getElementById("st-filter");
+		if(filter.style.display=="none"){
+			filter.style.display="";
 		}else{
-			btn.style.left="0px";
+			filter.style.display="none";
 		}
 		
 	}
-
+	
 	window.addEventListener('scroll',()=>{
 		let productLists = document.querySelector('.products-container');
 		const {scrollHeight,scrollTop,clientHeight} = document.documentElement;
@@ -152,8 +86,75 @@ get_header();
 	<div class="st-myshop-search" style="margin:auto; padding:10px 20px;">
 		<input class="st-myshop-search-box" id="st-search-box" name="Search" >
 		<div class="st-product-search-title" onclick="searchProducts()"><img src="<?php echo $path ?>/images/search.svg" loading="lazy" alt="" class="st-product-search-img"></div>
+
+		<div id="filterContainer" class="menu-main">
+			<div class="menu-container" id="st-filter" style="display:none">
+				<div class="menu-title">
+					<h3 class="menu-title-heading">Filter Menu</h3>
+				</div>
+				<div class="menu-list">
+					<div id="menuOptionList" class="menu-options">
+						<div class="menu-filters">	
+							<?php
+							if(isset($stFilterJson)){
+							$stFilters = json_decode($stFilterJson);
+								foreach ($stFilters as $filter) {
+								?>
+									<div class="menu-brand-select">
+									<div class="menu-option-block1">
+									<h4 class="menu-option-title"><?php echo $filter->name ?></h4>
+									</div>
+									<select name="<?php echo $filter->name ?>" key="<?php echo $filter->key ?>" id="<?php echo str_replace(" ","-",$filter->name) ?>" class="menu-option-select" <?php echo $filter->multi; ?>>
+									<?php foreach ($filter->values as $filterValue) {	?>
+									<option value="<?php echo $filterValue->value ?>"><?php echo $filterValue->name ?></option>
+									<?php } ?>
+									</select>
+									</div>
+
+									<?php
+								}
+							}
+							?>
+							<div class="menu-brand-select">
+								<div class="menu-option-block1">
+									<h4 class="menu-option-title">Sort By</h4>
+								</div>
+								<select name="sortBy" key="sortBy" id="sortBy" class="menu-option-select">
+									<option value="">None</option>
+									<option value="price">Price</option>
+									<option value="createdAt" selected>Latest</option>
+									<option value="quantitySold">Most Sold</option>
+								</select>
+							</div>
+							<div class="menu-brand-select">
+								<div class="menu-option-block1">
+									<h4 class="menu-option-title">Sort Order</h4>
+								</div>
+								<select name="sortOrder" key="orderBy" id="sortOrder" class="menu-option-select">
+									<option value="asc">ascending</option>
+									<option value="desc" selected>descending</option>
+								</select>
+								<select name="sortOrder" key="currency" id="sortOrder" class="menu-option-select" hidden>
+									<option value="<?php echo $stDefaultCurrency;?>"><?php echo $stDefaultCurrency;?></option>
+								</select>		 
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="menu-apply-div">
+					<div class="menu-apply-button">
+						<h3 class="menu-apply-button-lable" onclick="clearFilters()">Reset</h3>
+					</div>
+					<div class="menu-apply-button">
+						<h3 class="menu-apply-button-lable" onclick="filterProducts()">Apply &amp; Refresh</h3>
+					</div>
+				</div>
+			</div>
+			<div class="st-filter-btn" onclick="toggleFilter()"><img src="<?php echo $path ?>/images/Filter-Icon.png" loading="lazy" alt="" class="st-filter-img"><div>Filter</div></div>
+		</div>
+
 	</div>
-	<div count="10" imageSize="200x0" loadmore class="products-container" id="st-product-search-results">
+	<div count="10" imageSize="200x0" loadmore class="products-container" sortBy="createdAt" orderBy="desc" id="st-product-search-results" >
 		<div class="product-container single-product" style="display: none;" id="st-product-select-template">
 			<div class="product-image">
 				<a href="demo/awake/pdp/?product-id={{productId}}" class="am-product-link">
